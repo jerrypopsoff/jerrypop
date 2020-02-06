f83413a89a5770f3a01d1b5c31f7438a5fab33db
Gerald Haagsma <haagsmag@gmail.com>
2020-02-05 23:06:08 -0800

    Migrate Kernel component to styled components

--- src/components/Kernel.jsx
+++ src/components/Kernel.jsx
@@ -1,8 +1,40 @@
-import '../styles/Kernel.scss';
 import React from 'react';
 import { animateKernel } from '../utilities/Kernel';
 import { getRandomInteger } from '../utilities/Random';
 import kernel from '../images/Kernel.png';
+import styled from 'styled-components';
+
+const Img = styled.img`
+  animation: ${props =>
+      props.isRotatingClockwise ? 'kernel' : 'reverse-kernel'}
+    0.5s infinite linear;
+  height: 3vmin;
+  position: absolute;
+
+  @keyframes kernel {
+    0% {
+      transform: rotate(0deg);
+    }
+    50% {
+      transform: rotate(180deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+
+  @keyframes reverse-kernel {
+    0% {
+      transform: rotate(0deg);
+    }
+    50% {
+      transform: rotate(-180deg);
+    }
+    100% {
+      transform: rotate(-360deg);
+    }
+  }
+`;
 
 export default function Kernel({ delay, id, isRotatingClockwise, left, top }) {
   const style = { display: 'none', left: `${left}px`, top: `${top}px` };
@@ -10,15 +42,15 @@ export default function Kernel({ delay, id, isRotatingClockwise, left, top }) {
   setTimeout(() => animateKernel(id), delay);
 
   return (
-    <img
+    <Img
       alt="Popcorn kernel"
       aria-hidden="true"
-      className={isRotatingClockwise ? 'Kernel' : 'Kernel Kernel--reverse'}
       id={id}
+      isRotatingClockwise={isRotatingClockwise}
       src={kernel}
       style={style}
-      x-velocity={getRandomInteger(-15, 15)}
-      y-velocity={getRandomInteger(-20, -10)}
+      data-x-velocity={getRandomInteger(-15, 15)}
+      data-y-velocity={getRandomInteger(-20, -10)}
     />
   );
 }
