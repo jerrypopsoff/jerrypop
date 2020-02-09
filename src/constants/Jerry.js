247652a30d8b01bb9e04fd47d431ff7d1ff888a0
Gerald Haagsma <haagsmag@gmail.com>
2020-02-09 09:48:38 -0800

    Prettify recipes and add pad thai

--- src/constants/Jerry.js
+++ src/constants/Jerry.js
@@ -1,246 +1,439 @@
 export const USER_ID = 'IfGqGIjqLIP4soMIrBI9ULIUmUy2';
 
-export const RECIPES = [{
-  title: 'Rosemary Lemon Pepper',
-  time: '5-8 minutes',
-  materials: [{
-    name: 'Popcorn kernels',
-    amount: '1 cup'
-  }, {
-    name: 'Olive oil',
-    amount: '0.25 cups'
-  }, {
-    name: 'Fresh rosemary',
-    amount: '2 teaspoons, finely chopped'
-  }, {
-    name: 'Fresh lemon juice',
-    amount: '2 teaspoons'
-  }, {
-    name: 'Freshly ground black pepper',
-    amount: '2 teaspoons'
-  }, {
-    name: 'Garlic powder',
-    amount: '0.5 tablespoons'
-  }, {
-    name: 'Onion powder',
-    amount: '0.5 tablespoons'
-  }, {
-    name: 'Sage',
-    amount: '1 teaspoon'
-  }, {
-    name: 'Coarse kosher salt',
-    amount: 'To taste'
-  }],
-  steps: [{
-    content: 'Air pop a batch into your massive bowl'
-  }, {
-    content: 'Thoroughly mix rosemary, lemon juice, black pepper, garlic powder, onion powder, and sage into olive oil',
-    aside: 'The lemon juice will separate from the oil, but I promise this is okay'
-  }, {
-    content: 'Drizzle oil mixture as evenly as possible onto the popcorn',
-  }, {
-    content: 'Shake the shit out of that bowl',
-    aside: 'Popcorn tastes way better after it picks up that kitchen floor flavor, so don\'t worry about it spilling out of your colossal bowl'
-  }, {
-    content: 'Salt to taste, and shake the batch to mix'
-  }, {
-    content: 'Eat (or drink) popcorn'
-  }]
-}, {
-  title: 'PB&J',
-  time: '30 minutes',
-  materials: [{
-    name: 'Popcorn kernels',
-    amount: '1 cup'
-  }, {
-    name: 'Unsalted butter',
-    amount: '0.25 cup'
-  }, {
-    name: 'Sugar',
-    amount: '1.5 tablespoons'
-  }, {
-    name: 'Peanut butter',
-    amount: '3 tablespoons'
-  }, {
-    name: 'Grape jelly',
-    amount: '3 tablespoons'
-  }, {
-    name: 'Coarse kosher salt',
-    amount: 'To taste'
-  }],
-  steps: [{
-    content: 'Preheat oven to 290F'
-  }, {
-    content: 'Air pop a batch into your massive bowl'
-  }, {
-    content: 'Melt unsalted butter in a saucepan on medium heat'
-  }, {
-    content: 'Add sugar into saucepan and stir until mixed well'
-  }, {
-    content: 'Add peanut butter and jelly to the saucepan and increase to medium/high heat'
-  }, {
-    content: 'Stir frequently until you have a bubbling, brown paste/liquid thing with no chunks of jelly'
-  }, {
-    content: 'Drizzle in a spiral or zigzag motion into the popcorn',
-    aside: 'I prefer spiral, but this is where you can let your creativity shine'
-  }, {
-    content: 'Shake/stir the popcorn until evenly coated with the goods'
-  }, {
-    content: 'Salt to taste, and shake/stir the batch to mix'
-  }, {
-    content: 'Spread popcorn evenly on a tinfoil-lined baking sheet',
-    aside: 'Popcorn heap should not accumulate more than 1-2 inches in height in any area of the pan'
-  }, {
-    content: 'Bake for 16 minutes at 290F, stirring after 8 minutes'
-  }, {
-    content: 'Remove from oven and allow to cool 😎'
-  }]
-}, {
-  title: 'Cilantro Lime Taco',
-  time: '5-8 minutes',
-  materials: [{
-    name: 'Popcorn kernels',
-    amount: '1 cup'
-  }, {
-    name: 'Canola oil',
-    amount: '0.25 cups'
-  }, {
-    name: 'Fresh cilantro',
-    amount: '0.5 tablespoons, finely chopped'
-  }, {
-    name: 'Fresh lime',
-    amount: '0.125-0.25 limes (1 wedge)'
-  }, {
-    name: 'Cumin',
-    amount: '1 tablespoon'
-  }, {
-    name: 'Garlic powder',
-    amount: '0.5 tablespoons'
-  }, {
-    name: 'Onion powder',
-    amount: '0.5 tablespoons'
-  }, {
-    name: 'Chili powder',
-    amount: '1 teaspoon'
-  }, {
-    name: 'Chipotle powder',
-    amount: '1 teaspoon'
-  }, {
-    name: 'Cayenne pepper',
-    amount: 'to taste (1-2 teaspoons)'
-  }, {
-    name: 'Coarse kosher salt',
-    amount: 'To taste'
-  }],
-  steps: [{
-    content: 'Air pop a batch into your massive bowl'
-  }, {
-    content: 'Thoroughly mix cumin, garlic powder, onion powder, chili powder, chipotle powder, and cayenne pepper into canola oil'
-  }, {
-    content: 'Drizzle oil mixture in a spiral or zigzag motion into the popcorn',
-    aside: 'I prefer spiral, but this is where you can let your creativity shine'
-  }, {
-    content: 'Shake the shit out of that bowl',
-    aside: 'Popcorn tastes way better after it picks up that kitchen floor flavor, so don\'t worry about it spilling out of your colossal bowl'
-  }, {
-    content: 'Salt to taste, add cilantro, and shake the batch to mix'
-  }, {
-    content: 'Squeeze lime wedge in a spiral or zigzag motion into the popcorn',
-    aside: 'I prefer spiral, but this is where you get to be a special snowflake'
-  }, {
-    content: 'Eat it 😛'
-  }]
-}, {
-  title: 'Cranked Curry Basil',
-  time: '5-8 minutes',
-  materials: [{
-    name: 'Popcorn kernels',
-    amount: '1 cup'
-  }, {
-    name: 'Canola oil',
-    amount: '0.25 cups'
-  }, {
-    name: 'Fresh basil',
-    amount: '0.5 tablespoons, finely chopped'
-  }, {
-    name: 'Your favorite curry powder',
-    amount: '1 tablespoon'
-  }, {
-    name: 'Garam masala',
-    amount: '1 teaspoon'
-  }, {
-    name: 'Garlic powder',
-    amount: '0.5 tablespoons'
-  }, {
-    name: 'Onion powder',
-    amount: '0.5 tablespoons'
-  }, {
-    name: 'Cayenne pepper',
-    amount: '1-2 teaspoons'
-  }, {
-    name: 'Coarse kosher salt',
-    amount: 'To taste'
-  }],
-  steps: [{
-    content: 'Air pop a batch into your massive bowl'
-  }, {
-    content: 'Thoroughly mix basil, curry powder, garam masala, garlic powder, onion powder, and cayenne pepper into canola oil',
-    aside: 'Optionally leave half the basil aside to add as a garnish at the end'
-  }, {
-    content: 'Drizzle oil mixture in a spiral or zigzag motion into the popcorn',
-    aside: 'I prefer spiral, but this is where you can let your creativity shine'
-  }, {
-    content: 'Shake the shit out of that bowl',
-    aside: 'Popcorn tastes way better after it picks up that kitchen floor flavor, so don\'t worry about it spilling out of your colossal bowl'
-  }, {
-    content: 'Salt to taste, and shake the batch to mix'
-  }, {
-    content: '😛'
-  }]
-}, {
-  title: 'Chocolate Hazelnut',
-  time: '30 minutes',
-  materials: [{
-    name: 'Popcorn kernels',
-    amount: '1 cup'
-  }, {
-    name: 'Unsalted butter',
-    amount: '0.25 cup'
-  }, {
-    name: 'Sugar',
-    amount: '1.5 tablespoons'
-  }, {
-    name: 'Nutella',
-    amount: '4 tablespoons'
-  }, {
-    name: 'Coarse kosher salt',
-    amount: 'To taste'
-  }],
-  steps: [{
-    content: 'Preheat oven to 290F'
-  }, {
-    content: 'Air pop a batch into your massive bowl'
-  }, {
-    content: 'Melt unsalted butter in a saucepan on medium heat'
-  }, {
-    content: 'Add sugar into saucepan and stir until mixed well'
-  }, {
-    content: 'Add Nutella to the saucepan and increase to medium/high heat'
-  }, {
-    content: 'Stir frequently until you have a bubbling, brown paste/liquid thing'
-  }, {
-    content: 'Drizzle in a spiral or zigzag motion into the popcorn',
-    aside: 'I prefer spiral, but this is where you can let your creativity shine'
-  }, {
-    content: 'Shake/stir the popcorn until evenly coated with the goods'
-  }, {
-    content: 'Salt to taste, and shake/stir the batch to mix'
-  }, {
-    content: 'Spread popcorn evenly on a tinfoil-lined baking sheet',
-    aside: 'Popcorn heap should not accumulate more than 1-2 inches in height in any area of the pan'
-  }, {
-    content: 'Bake for 16 minutes at 290F, stirring after 8 minutes'
-  }, {
-    content: 'Remove from oven and allow to cool 😎'
-  }]
-}];
+export const RECIPES = [
+  {
+    title: 'Rosemary Lemon Pepper',
+    time: '5-8 minutes',
+    materials: [
+      {
+        name: 'Popcorn kernels',
+        amount: '1 cup',
+      },
+      {
+        name: 'Olive oil',
+        amount: '0.33 cups',
+      },
+      {
+        name: 'Fresh rosemary',
+        amount: '2 teaspoons, finely chopped',
+      },
+      {
+        name: 'Fresh lemon juice',
+        amount: '2 teaspoons',
+      },
+      {
+        name: 'Freshly ground black pepper',
+        amount: '2 teaspoons',
+      },
+      {
+        name: 'Garlic powder',
+        amount: '0.5 tablespoons',
+      },
+      {
+        name: 'Onion powder',
+        amount: '0.5 tablespoons',
+      },
+      {
+        name: 'Sage',
+        amount: '1 teaspoon',
+      },
+      {
+        name: 'Coarse kosher salt',
+        amount: 'To taste',
+      },
+    ],
+    steps: [
+      {
+        content: 'Air pop a batch into your massive bowl',
+      },
+      {
+        content:
+          'Thoroughly mix rosemary, lemon juice, black pepper, garlic powder, onion powder, and sage into olive oil',
+        aside:
+          'The lemon juice will separate from the oil, but I promise this is okay',
+      },
+      {
+        content: 'Drizzle oil mixture as evenly as possible onto the popcorn',
+      },
+      {
+        content: 'Shake the shit out of that bowl',
+        aside: `Popcorn tastes way better after it picks up that kitchen floor flavor, so don't worry about it spilling out of your colossal bowl`,
+      },
+      {
+        content: 'Salt to taste, and shake the batch to mix',
+      },
+      {
+        content: 'Eat (or drink) popcorn',
+      },
+    ],
+  },
+  {
+    title: 'PB&J',
+    time: '30 minutes',
+    materials: [
+      {
+        name: 'Popcorn kernels',
+        amount: '1 cup',
+      },
+      {
+        name: 'Unsalted butter',
+        amount: '0.25 cup',
+      },
+      {
+        name: 'Sugar',
+        amount: '1.5 tablespoons',
+      },
+      {
+        name: 'Peanut butter',
+        amount: '3 tablespoons',
+      },
+      {
+        name: 'Grape jelly',
+        amount: '3 tablespoons',
+      },
+      {
+        name: 'Coarse kosher salt',
+        amount: 'To taste',
+      },
+    ],
+    steps: [
+      {
+        content: 'Preheat oven to 290F',
+      },
+      {
+        content: 'Air pop a batch into your massive bowl',
+      },
+      {
+        content: 'Melt unsalted butter in a saucepan on medium heat',
+      },
+      {
+        content: 'Add sugar into saucepan and stir until mixed well',
+      },
+      {
+        content:
+          'Add peanut butter and jelly to the saucepan and increase to medium/high heat',
+      },
+      {
+        content:
+          'Stir frequently until you have a bubbling, brown paste/liquid thing with no chunks of jelly',
+      },
+      {
+        content: 'Drizzle in a spiral or zigzag motion into the popcorn',
+        aside:
+          'I prefer spiral, but this is where you can let your creativity shine',
+      },
+      {
+        content: 'Shake/stir the popcorn until evenly coated with the goods',
+      },
+      {
+        content: 'Salt to taste, and shake/stir the batch to mix',
+      },
+      {
+        content: 'Spread popcorn evenly on a tinfoil-lined baking sheet',
+        aside:
+          'Popcorn heap should not accumulate more than 1-2 inches in height in any area of the pan',
+      },
+      {
+        content: 'Bake for 16 minutes at 290F, stirring after 8 minutes',
+      },
+      {
+        content: 'Remove from oven and allow to cool 😎',
+      },
+    ],
+  },
+  {
+    title: 'Cilantro Lime Taco',
+    time: '5-8 minutes',
+    materials: [
+      {
+        name: 'Popcorn kernels',
+        amount: '1 cup',
+      },
+      {
+        name: 'Canola oil',
+        amount: '0.33 cups',
+      },
+      {
+        name: 'Fresh cilantro',
+        amount: '0.5 tablespoons, finely chopped',
+      },
+      {
+        name: 'Fresh lime',
+        amount: '0.125-0.25 limes (1 wedge)',
+      },
+      {
+        name: 'Cumin',
+        amount: '1 tablespoon',
+      },
+      {
+        name: 'Garlic powder',
+        amount: '0.5 tablespoons',
+      },
+      {
+        name: 'Onion powder',
+        amount: '0.5 tablespoons',
+      },
+      {
+        name: 'Chili powder',
+        amount: '1 teaspoon',
+      },
+      {
+        name: 'Chipotle powder',
+        amount: '1 teaspoon',
+      },
+      {
+        name: 'Cayenne pepper',
+        amount: 'to taste (1-2 teaspoons)',
+      },
+      {
+        name: 'Coarse kosher salt',
+        amount: 'To taste',
+      },
+    ],
+    steps: [
+      {
+        content: 'Air pop a batch into your massive bowl',
+      },
+      {
+        content:
+          'Thoroughly mix cumin, garlic powder, onion powder, chili powder, chipotle powder, and cayenne pepper into canola oil',
+      },
+      {
+        content:
+          'Drizzle oil mixture in a spiral or zigzag motion into the popcorn',
+        aside:
+          'I prefer spiral, but this is where you can let your creativity shine',
+      },
+      {
+        content: 'Shake the shit out of that bowl',
+        aside: `Popcorn tastes way better after it picks up that kitchen floor flavor, so don't worry about it spilling out of your colossal bowl`,
+      },
+      {
+        content: 'Salt to taste, add cilantro, and shake the batch to mix',
+      },
+      {
+        content:
+          'Squeeze lime wedge in a spiral or zigzag motion into the popcorn',
+        aside:
+          'I prefer spiral, but this is where you get to be a special snowflake',
+      },
+      {
+        content: 'Eat it 😛',
+      },
+    ],
+  },
+  {
+    title: 'Cranked Curry Basil',
+    time: '5-8 minutes',
+    materials: [
+      {
+        name: 'Popcorn kernels',
+        amount: '1 cup',
+      },
+      {
+        name: 'Canola oil',
+        amount: '0.33 cups',
+      },
+      {
+        name: 'Fresh basil',
+        amount: '0.5 tablespoons, finely chopped',
+      },
+      {
+        name: 'Your favorite curry powder',
+        amount: '1 tablespoon',
+      },
+      {
+        name: 'Garam masala',
+        amount: '1 teaspoon',
+      },
+      {
+        name: 'Garlic powder',
+        amount: '0.5 tablespoons',
+      },
+      {
+        name: 'Onion powder',
+        amount: '0.5 tablespoons',
+      },
+      {
+        name: 'Cayenne pepper',
+        amount: '1-2 teaspoons',
+      },
+      {
+        name: 'Coarse kosher salt',
+        amount: 'To taste',
+      },
+    ],
+    steps: [
+      {
+        content: 'Air pop a batch into your massive bowl',
+      },
+      {
+        content:
+          'Thoroughly mix basil, curry powder, garam masala, garlic powder, onion powder, and cayenne pepper into canola oil',
+        aside:
+          'Optionally leave half the basil aside to add as a garnish at the end',
+      },
+      {
+        content:
+          'Drizzle oil mixture in a spiral or zigzag motion into the popcorn',
+        aside:
+          'I prefer spiral, but this is where you can let your creativity shine',
+      },
+      {
+        content: 'Shake the shit out of that bowl',
+        aside: `Popcorn tastes way better after it picks up that kitchen floor flavor, so don't worry about it spilling out of your colossal bowl`,
+      },
+      {
+        content: 'Salt to taste, and shake the batch to mix',
+      },
+      {
+        content: '😛',
+      },
+    ],
+  },
+  {
+    title: 'Chocolate Hazelnut',
+    time: '30 minutes',
+    materials: [
+      {
+        name: 'Popcorn kernels',
+        amount: '1 cup',
+      },
+      {
+        name: 'Unsalted butter',
+        amount: '0.25 cup',
+      },
+      {
+        name: 'Sugar',
+        amount: '1.5 tablespoons',
+      },
+      {
+        name: 'Nutella',
+        amount: '4 tablespoons',
+      },
+      {
+        name: 'Coarse kosher salt',
+        amount: 'To taste',
+      },
+    ],
+    steps: [
+      {
+        content: 'Preheat oven to 290F',
+      },
+      {
+        content: 'Air pop a batch into your massive bowl',
+      },
+      {
+        content: 'Melt unsalted butter in a saucepan on medium heat',
+      },
+      {
+        content: 'Add sugar into saucepan and stir until mixed well',
+      },
+      {
+        content: 'Add Nutella to the saucepan and increase to medium/high heat',
+      },
+      {
+        content:
+          'Stir frequently until you have a bubbling, brown paste/liquid thing',
+      },
+      {
+        content: 'Drizzle in a spiral or zigzag motion into the popcorn',
+        aside:
+          'I prefer spiral, but this is where you can let your creativity shine',
+      },
+      {
+        content: 'Shake/stir the popcorn until evenly coated with the goods',
+      },
+      {
+        content: 'Salt to taste, and shake/stir the batch to mix',
+      },
+      {
+        content: 'Spread popcorn evenly on a tinfoil-lined baking sheet',
+        aside:
+          'Popcorn heap should not accumulate more than 1-2 inches in height in any area of the pan',
+      },
+      {
+        content: 'Bake for 16 minutes at 290F, stirring after 8 minutes',
+      },
+      {
+        content: 'Remove from oven and allow to cool 😎',
+      },
+    ],
+  },
+  {
+    title: 'Pad Thai',
+    time: '8-10 minutes',
+    materials: [
+      {
+        name: 'Popcorn kernels',
+        amount: '1 cup',
+      },
+      {
+        name: 'Canola oil',
+        amount: '0.5 cups',
+      },
+      {
+        name: 'Your favorite thai curry powder',
+        amount: '1.5 tablespoons',
+      },
+      {
+        name: 'Garlic powder',
+        amount: '0.5 tablespoons',
+      },
+      {
+        name: 'Onion powder',
+        amount: '0.5 tablespoons',
+      },
+      {
+        name: 'Cayenne pepper',
+        amount: '1-2 teaspoons',
+      },
+      {
+        name: 'Fresh garlic chives (or green onion)',
+        amount: '0.5 tablespoons, finely chopped',
+      },
+      {
+        name: 'Peanuts',
+        amount: '0.25-0.5 cups',
+      },
+      {
+        name: 'Crystallized lime (or lime juice powder)',
+        amount: '1-2 teaspoons',
+      },
+      {
+        name: 'Coarse kosher salt',
+        amount: 'To taste',
+      },
+    ],
+    steps: [
+      {
+        content: 'Air pop a batch into your massive bowl',
+      },
+      {
+        content: 'Finely grind peanuts in a blender or food processor',
+      },
+      {
+        content:
+          'Thoroughly mix thai curry powder, garlic powder, onion powder, cayenne pepper, garlic chives, 1 tablespoon of ground peanuts, and crystallized lime into canola oil',
+        aside:
+          'Optionally leave half the garlic chives aside to add as a garnish at the end',
+      },
+      {
+        content:
+          'Drizzle oil mixture in a spiral or zigzag motion into the popcorn',
+        aside:
+          'I prefer spiral, but this is where you can let your creativity shine',
+      },
+      {
+        content: 'Shake the shit out of that bowl',
+        aside: `Popcorn tastes way better after it picks up that kitchen floor flavor, so don't worry about it spilling out of your colossal bowl`,
+      },
+      {
+        content: 'Salt to taste, and shake the batch to mix',
+      },
+      {
+        content: '😛',
+      },
+    ],
+  },
+];
