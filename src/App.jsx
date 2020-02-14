450e1b31bed9f73c49f45cbc30b1f13e2f3851be
Gerald Haagsma <haagsmag@gmail.com>
2020-02-13 23:37:09 -0800

    Add not found page
    -Add not found route and page
    -Move Footer to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import Footer from './components/Footer';
 import Home from './components/Home';
 import React from 'react';
 import User from './components/User';
@@ -7,13 +8,29 @@ export default function App() {
   return (
     <Router>
       <Switch>
+        <Route exact path="/">
+          <Home heading="Poppin’ off with Jerry">
+            Pick up an{' '}
+            <a
+              href="https://www.amazon.com/gp/product/B01G7DM3G2/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B01G7DM3G2&linkCode=as2&tag=jerry04c-20&linkId=ca82b9da5246d1a9a2e398a2d6be50c2"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              air popper
+            </a>{' '}
+            and an enormous bowl before you do anything else.
+          </Home>
+        </Route>
         <Route path="/user">
           <User />
         </Route>
-        <Route path="/">
-          <Home />
+        <Route path="*">
+          <Home heading="Whoops! Someone spilled the kernels.">
+            That page does not exist.
+          </Home>
         </Route>
       </Switch>
+      <Footer />
     </Router>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,37 +10,25 @@ import {
   StyledLogo,
 } from '../styled-components/Home';
 import { getFountainLeft, getFountainTop } from '../utilities/KernelFountain';
-import Footer from './Footer';
 import InteractiveLink from './InteractiveLink';
 import KernelFountain from './KernelFountain';
 import React from 'react';
 import logo from '../images/Jerrypop.png';
 import useWindowSize from '@rehooks/window-size';
 
-export default function Home() {
+export default function Home({ children, heading }) {
   const windowSize = useWindowSize();
 
   return (
     <StyledHome>
       <StyledLogo src={logo} alt="Jerrypop Logo" />
-      <StyledHeading>Poppin’ off with Jerry</StyledHeading>
-      <StyledContent>
-        Pick up an{' '}
-        <a
-          href="https://www.amazon.com/gp/product/B01G7DM3G2/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B01G7DM3G2&linkCode=as2&tag=jerry04c-20&linkId=ca82b9da5246d1a9a2e398a2d6be50c2"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          air popper
-        </a>{' '}
-        and an enormous bowl before you do anything else.
-      </StyledContent>
+      <StyledHeading>{heading}</StyledHeading>
+      <StyledContent>{children}</StyledContent>
       <InteractiveLink
         defaultText="🍿 Start poppin’ 🍿"
         interactiveText="💥 Start poppin’ 💥"
         to="/user/IfGqGIjqLIP4soMIrBI9ULIUmUy2"
       />
-      <Footer />
       <KernelFountain
         delay={KERNEL_FOUNTAIN_DELAY}
         duration={KERNEL_FOUNTAIN_DURATION}
