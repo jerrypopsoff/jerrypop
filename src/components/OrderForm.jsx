bc5aeec8a693c8605634364378bf528ebc807d5f
Gerald Haagsma <haagsmag@gmail.com>
2022-06-23 16:14:34 -0700

    Update order request form to be mobile friendly

--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import useWindowSize from '@rehooks/window-size';
 
 export default function OrderForm() {
+  const defaultWidth = 640;
+  const { innerWidth } = useWindowSize();
+  const width = Math.min(innerWidth - 24, defaultWidth);
+
   return (
     <iframe
       frameBorder="0"
-      height="1920"
+      height={width < defaultWidth ? 2100 : 1920}
       marginHeight="0"
       marginWidth="0"
       src="https://docs.google.com/forms/d/e/1FAIpQLSdXm4aTzZVHn5RIYhC1xLy-MNtyRxohuU2tZCH-TPnU8GqGHw/viewform?embedded=true"
       style={{ backgroundColor: '#edeeff', borderRadius: 5 }}
       title="Jerrypop Order Form"
-      width="640"
+      width={width}
     >
       Loading…
     </iframe>
