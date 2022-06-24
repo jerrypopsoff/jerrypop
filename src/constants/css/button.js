29275377ca37286382475fe47855ef0fbd254b2c
Gerald Haagsma <haagsmag@gmail.com>
2022-06-23 19:40:34 -0700

    Style all buttons consistently
    - Remove interactive text
    - Apply consistent font size, width, and animation to all buttons, navigation links, and recipe cards

--- src/constants/css/button.js
+++ src/constants/css/button.js
@@ -3,15 +3,22 @@ import { css } from 'styled-components';
 export const buttonStyle = css`
   align-items: center;
   background-color: #edeeff;
+  border: none;
   border-radius: 4px;
   color: #222;
   cursor: pointer;
   display: flex;
-  font-size: 36px;
+  font-size: 24px;
   font-weight: bold;
   justify-content: center;
   letter-spacing: 0.25px;
   margin: 8px;
-  padding: 24px;
+  min-width: 240px;
+  padding: 24px 0;
   text-transform: uppercase;
+
+  &:hover,
+  &:focus {
+    color: #444;
+  }
 `;
