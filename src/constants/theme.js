6b53b7d141ea51596d849261655e6f025e328753
Gerald Haagsma <haagsmag@gmail.com>
2022-07-14 02:04:31 -0700

    Align packaging mocks with website color scheme

--- src/constants/theme.js
+++ src/constants/theme.js
@@ -1,19 +1,19 @@
 export const THEME = {
   // PB&J
   BLUE_PINK: {
-    mainThemeDark: '#2b6dbe',
-    mainThemeMedium: '#e2279d',
+    mainThemeDark: '#0072da',
+    mainThemeMedium: '#ff409e',
     textHighlight: 'var(--main-theme-light)',
   },
   PINK_PURPLE: {
-    mainThemeDark: '#e2279d',
+    mainThemeDark: '#ff409e',
     mainThemeMedium: '#835dd0',
     textHighlight: 'var(--main-theme-light)',
   },
   // Habanero Ranch
   CYAN_YELLOW: {
-    mainThemeDark: '#57c7cb',
-    mainThemeMedium: '#f0ec2e',
+    mainThemeDark: '#2dccd3',
+    mainThemeMedium: '#f7ea48',
     textHighlight: '#000',
   },
   PURPLE_GREEN: {
@@ -24,28 +24,28 @@ export const THEME = {
   // Pad Thai?
   ORANGE_PINK: {
     mainThemeDark: '#f2a900',
-    mainThemeMedium: '#e2279d',
+    mainThemeMedium: '#ff409e',
     textHighlight: 'var(--main-theme-light)',
   },
   // Rosemary Lemon Pepper
   GREEN_YELLOW: {
     mainThemeDark: '#00b140',
-    mainThemeMedium: '#f0ec2e',
+    mainThemeMedium: '#f7ea48',
     textHighlight: '#000',
   },
   // BLUE_CYAN: {
-  //   mainThemeDark: '#2b6dbe',
-  //   mainThemeMedium: '#57c7cb',
+  //   mainThemeDark: '#0072da',
+  //   mainThemeMedium: '#2dccd3',
   //   textHighlight: 'var(--main-theme-light)',
   // },
   // VIOLET_CYAN: {
   //   mainThemeDark: '#9678d3',
-  //   mainThemeMedium: '#57c7cb',
+  //   mainThemeMedium: '#2dccd3',
   //   textHighlight: '#000',
   // },
   // PURPLE_CYAN: {
   //   mainThemeDark: '#835dd0',
-  //   mainThemeMedium: '#57c7cb',
+  //   mainThemeMedium: '#2dccd3',
   //   textHighlight: '#000',
   // },
 };
