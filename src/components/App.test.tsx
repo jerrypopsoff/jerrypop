6f2539a2b8c6b5e78427dac5e162058b06fae27c
Gerald Haagsma <haagsmag@gmail.com>
2022-07-29 18:13:31 -0700

    Add integration tests for navigation menu

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -66,3 +66,37 @@ describe('theme', () => {
   });
 });
 
+describe('navigation menu', () => {
+  test('renders navigation menu items based on current route', async () => {
+    const { getAllByRole, getByAltText } = render(<App />);
+
+    // Home route
+    let linkElements = getAllByRole('link');
+    expect(linkElements.length).toEqual(5);
+    expect(linkElements[0]).toHaveTextContent('Purchase');
+    expect(linkElements[1]).toHaveTextContent('Recipes');
+
+    // Purchase route
+    await userEvent.click(linkElements[0]);
+
+    linkElements = getAllByRole('link');
+    expect(linkElements.length).toEqual(8);
+    expect(linkElements[0]).toHaveTextContent('');
+    expect(linkElements[1]).toHaveTextContent('info@jerrypop.com');
+    expect(linkElements[2]).toHaveTextContent('info@jerrypop.com');
+    expect(linkElements[3]).toHaveTextContent('Home');
+    expect(linkElements[4]).toHaveTextContent('Recipes');
+
+    // Recipes route
+    await userEvent.click(linkElements[4]);
+
+    linkElements = getAllByRole('link');
+    expect(linkElements.length).toEqual(6);
+    expect(linkElements[0]).toHaveTextContent('');
+    expect(linkElements[1]).toHaveTextContent('Home');
+    expect(linkElements[2]).toHaveTextContent('Purchase');
+
+    // Reset router state for next test
+    await userEvent.click(getByAltText('Jerrypop logo'));
+  });
+});
