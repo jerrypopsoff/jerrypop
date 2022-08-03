b538954aae9d9f8d841ef3b0b4510e1e1300f7f0
Gerald Haagsma <haagsmag@gmail.com>
2022-08-03 12:21:53 -0700

    Disable analytics requests for non-production environments

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  blockHosts: ['*google-analytics.com', '*googletagmanager.com'],
   projectId: 'ttcbvc',
   e2e: {
     setupNodeEvents(on, config) {
