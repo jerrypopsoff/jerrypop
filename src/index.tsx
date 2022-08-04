e3427d523469baed3848e1c017812df999dffcae
Gerald Haagsma <haagsmag@gmail.com>
2022-08-03 18:12:47 -0700

    Add web vital reporting
    Per https://bit.ly/CRA-vitals.

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { initializeClickPop } from './utilities/click-pop';
 import { logAsciiArt } from './utilities/console-ascii-art';
+import { reportWebVitals, sendToAnalytics } from './report-web-vitals';
 
 initializeClickPop(document);
 logAsciiArt();
 
-const rootElement = document.getElementById('root');
+createRoot(document.getElementById('root') as HTMLElement).render(<App />);
 
-if (rootElement === null) {
-  throw new Error('Root element not found in document');
-}
-
-createRoot(rootElement).render(<App />);
+reportWebVitals(sendToAnalytics);
---
+++ src/report-web-vitals.ts
@@ -0,0 +1,35 @@
+import type { Metric, ReportHandler } from 'web-vitals';
+
+export function sendToAnalytics({ id, name, value }: Metric) {
+  if (!(ga instanceof Function)) {
+    return;
+  }
+
+  ga('send', 'event', {
+    eventAction: name,
+    eventCategory: 'Web Vitals',
+    eventLabel: id,
+    eventValue: Math.round(name === 'CLS' ? value * 1000 : value),
+    nonInteraction: true,
+  });
+}
+
+/**
+ * Report web vitals. More information can be found at
+ * {@link https://bit.ly/CRA-vitals}.
+ */
+export async function reportWebVitals(onPerfEntry?: ReportHandler) {
+  if (!onPerfEntry || !(onPerfEntry instanceof Function)) {
+    return;
+  }
+
+  const { getCLS, getFID, getFCP, getLCP, getTTFB } = await import(
+    'web-vitals'
+  );
+
+  getCLS(onPerfEntry);
+  getFID(onPerfEntry);
+  getFCP(onPerfEntry);
+  getLCP(onPerfEntry);
+  getTTFB(onPerfEntry);
+}
