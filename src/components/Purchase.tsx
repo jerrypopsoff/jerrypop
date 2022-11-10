c520a5deb272a174766f832c4bc9afcbfa123147
Gerald Haagsma <haagsmag@gmail.com>
2022-11-09 16:35:22 -0800

    Add catering information to purchase page

--- src/components/Purchase.tsx
+++ src/components/Purchase.tsx
@@ -3,7 +3,6 @@ import { Helmet } from 'react-helmet-async';
 import NavigationMenu from './NavigationMenu';
 import OrderFormDialog from './OrderFormDialog';
 import Products from './Products';
-// import { RETAILERS } from '../constants/retailers';
 import Typography from './Typography';
 import styled from 'styled-components';
 
@@ -51,27 +50,11 @@ const Purchase: React.FC = () => {
             onCloseOrderOnlineDialog={onCloseOrderOnlineDialog}
           />
         )}
-        {/* <Typography type="h2">Retail</Typography>
+        <Typography type="h2">Catering</Typography>
         <Typography maxWidth="600px" type="p">
-          Products are not yet available for purchase from retailers. Interested
-          in carrying them at your retail location? Reach out to{' '}
-          <a href="mailto:info@jerrypop.com">info@jerrypop.com</a>.
-        </Typography> */}
-        {/* <Typography maxWidth="600px" type="p">
-        Products are available for purchase from the following retailers.
-        Interested in carrying them at your retail location? Reach out to{' '}
-        <a href="mailto:info@jerrypop.com">info@jerrypop.com</a>.
-      </Typography>
-      <ul>
-        {RETAILERS.map(retailer => (
-          <li key={retailer.displayName}>{retailer.displayName}</li>
-        ))}
-      </ul> */}
-        {/* <Typography type="h2">Catering</Typography>
-        <Typography maxWidth="600px" type="p">
-          Interested in serving craft popcorn at your event? Reach out to{' '}
-          <a href="mailto:info@jerrypop.com">info@jerrypop.com</a>.
-        </Typography> */}
+          Interested in serving locally handcrafted popcorn at your event? Reach
+          out to <a href="mailto:info@jerrypop.com">info@jerrypop.com</a>.
+        </Typography>
         <StyledNavigationMenu />
       </StyledPageContent>
     </>
