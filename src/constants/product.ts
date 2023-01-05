bb04ea38496db464aa27032db527a8e3f08b5046
Gerald Haagsma <haagsmag@gmail.com>
2023-01-05 13:46:17 -0800

    Remove popcorn kernel variety from product descriptions

--- src/constants/product.ts
+++ src/constants/product.ts
@@ -7,14 +7,14 @@ import { Product } from '../types/product';
 export const PRODUCTS: Product[] = [
   {
     description:
-      'Large and crunchy mushroom popcorn smothered in peanut butter and strawberry jam. A vegan-friendly blast of sweet and salty nostalgia. Locally popped, handcrafted, and packaged in a fully compostable bag.',
+      'Large and crunchy popcorn smothered in peanut butter and strawberry jam. A vegan-friendly blast of sweet and salty nostalgia. Locally popped, handcrafted, and packaged in a fully compostable bag.',
     labelImage: PbjFrontLabel,
     photograph: PbjPhotograph,
     title: 'PB&J Craft Popcorn',
   },
   {
     description:
-      'Hulless and crisp butterfly popcorn smothered in creamy, zesty ranch seasoning with a habanero kick. Locally popped, handcrafted, and packaged in a fully compostable bag.',
+      'Hulless and crisp popcorn smothered in creamy, zesty ranch seasoning with a habanero kick. Locally popped, handcrafted, and packaged in a fully compostable bag.',
     labelImage: HrFrontLabel,
     photograph: HrPhotograph,
     title: 'Habanero Ranch Craft Popcorn',
