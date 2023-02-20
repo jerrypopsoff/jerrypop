58eaa9d841256a52724e136f504590bdd3571a16
Gerald Haagsma <haagsmag@gmail.com>
2023-02-20 12:55:08 -0800

    Add design for consumer-facing business cards

--- src/components/Recipes.tsx
+++ src/components/Recipes.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Typography from './Typography';
 import styled from 'styled-components';
+import EmailIcon from '../images/email-icon.svg';
+import InstagramIcon from '../images/instagram-icon.svg';
+import InternetIcon from '../images/internet-icon.svg';
 
 const StyledBusinessCardFront = styled.div`
   background-color: white;
-  color: black;
+  color: #1b365f;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  padding: 70px 0;
   position: fixed;
   top: 0;
   left: 0;
@@ -12,15 +20,31 @@ const StyledBusinessCardFront = styled.div`
   right: 0;
 `;
 
-const StyledSpacer = styled.div`
-  height: 148px;
+const StyledWordmark = styled(Typography)`
+  font-family: 'Cooper Nouveau';
 `;
 
 const StyledContactInformation = styled(Typography)`
-  font-size: 36px;
-  font-weight: 300;
-  margin: 8px 0;
+  font-family: 'Brandon Text';
+  font-size: 48px;
+  font-weight: 400;
   text-shadow: none;
+`;
+
+const StyledSubwordmark = styled(StyledContactInformation)`
+  font-size: 36px;
+`;
+
+const StyledSocialLinkIcon = styled.img`
+  height: 40px;
+  margin: 8px 20px 0;
+  width: 40px;
+`;
+
+const StyledSocialLine = styled.div`
+  align-items: center;
+  display: flex;
+  margin: 8px 0;
 `;
 
 /**
@@ -29,29 +53,45 @@ const StyledContactInformation = styled(Typography)`
 const Recipes: React.FC = () => {
   return (
     <StyledBusinessCardFront>
-      <Typography
-        hasTextShadow={false}
-        fontSize={95}
-        margin="270px 0 -24px"
-        type="h1"
-      >
-        Jerry
-      </Typography>
-      <Typography hasTextShadow={false} fontSize={95} margin="0" type="h1">
-        Haagsma
-      </Typography>
-      <StyledContactInformation type="p">Owner & CEO</StyledContactInformation>
-      <StyledSpacer />
-      <StyledContactInformation type="p">415-570-8472</StyledContactInformation>
-      <StyledContactInformation type="p">
-        info@jerrypop.com
-      </StyledContactInformation>
-      <StyledContactInformation type="p">
-        www.jerrypop.com
-      </StyledContactInformation>
-      <StyledContactInformation type="p">
-        San Francisco, CA
-      </StyledContactInformation>
+      <div>
+        <StyledWordmark
+          hasTextShadow={false}
+          fontSize={120}
+          margin="0 0 -24px"
+          type="p"
+        >
+          Jerrypop
+        </StyledWordmark>
+        <StyledSubwordmark type="p">Craft Popcorn</StyledSubwordmark>
+      </div>
+      <div>
+        <StyledContactInformation margin="8px 0" type="p">
+          Catering · Retail · Pop-ups
+        </StyledContactInformation>
+        <StyledSubwordmark margin="8px 0" type="p">
+          San Francisco, CA
+        </StyledSubwordmark>
+      </div>
+      <div>
+        <StyledSocialLine>
+          <StyledSocialLinkIcon alt="Instagram icon" src={InstagramIcon} />
+          <StyledContactInformation margin="0" type="p">
+            @craftpopcorn
+          </StyledContactInformation>
+        </StyledSocialLine>
+        <StyledSocialLine>
+          <StyledSocialLinkIcon alt="Email icon" src={EmailIcon} />
+          <StyledContactInformation margin="0" type="p">
+            info@jerrypop.com
+          </StyledContactInformation>
+        </StyledSocialLine>
+        <StyledSocialLine>
+          <StyledSocialLinkIcon alt="Website icon" src={InternetIcon} />
+          <StyledContactInformation margin="0" type="p">
+            jerrypop.com
+          </StyledContactInformation>
+        </StyledSocialLine>
+      </div>
     </StyledBusinessCardFront>
   );
 };
