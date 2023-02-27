a1edb8c1278900bb2857b8b68f224fcc67ea7454
Gerald Haagsma <haagsmag@gmail.com>
2023-02-27 12:57:10 -0800

    Add Washington Post article to press page

--- src/components/Press.tsx
+++ src/components/Press.tsx
@@ -8,6 +8,39 @@ import Baklava2400 from '../images/glamorous-baklava-2400.jpeg';
 import BaklavaWebp600 from '../images/glamorous-baklava-600.webp';
 import BaklavaWebp1200 from '../images/glamorous-baklava-1200.webp';
 import BaklavaWebp2400 from '../images/glamorous-baklava-2400.webp';
+import styled from 'styled-components';
+import {
+  StyledTileImage,
+  StyledTileImageContainer,
+  StyledTileInformation,
+  StyledTileList,
+  StyledTileListItem,
+} from '../constants/css/tile-list';
+import { TILE_LIST_BREAKPOINT_WIDTH_PX } from '../constants/breakpoint';
+import { ARTICLES } from '../constants/press';
+
+const StyledListItem = styled.li`
+  width: 100%;
+`;
+
+const StyledArticleListItem = styled(StyledTileListItem)`
+  margin: 0 auto;
+  padding: 32px 24px;
+  text-decoration: none;
+
+  @media (max-width: ${TILE_LIST_BREAKPOINT_WIDTH_PX}px) {
+    padding: 32px 0;
+  }
+`;
+
+const StyledArticlePublication = styled(Typography)`
+  margin: 0 0 12px;
+  text-transform: uppercase;
+`;
+
+const StyledArticleTitle = styled(Typography)`
+  margin: 0;
+`;
 
 const Press: React.FC = () => {
   return (
@@ -23,6 +56,30 @@ const Press: React.FC = () => {
         srcSet={`${BaklavaWebp600} 600w, ${BaklavaWebp1200} 1200w, ${BaklavaWebp2400} 2400w`}
       />
       <Typography type="h1">Press</Typography>
+      <StyledTileList>
+        {ARTICLES.map((article) => (
+          <StyledListItem>
+            <StyledArticleListItem
+              as="a"
+              href={article.href}
+              rel="noreferrer"
+              target="_blank"
+            >
+              <StyledTileImageContainer>
+                <StyledTileImage src={article.image} />
+              </StyledTileImageContainer>
+              <StyledTileInformation>
+                <StyledArticlePublication type="p">
+                  {article.publication}
+                </StyledArticlePublication>
+                <StyledArticleTitle type="h2">
+                  {article.title}
+                </StyledArticleTitle>
+              </StyledTileInformation>
+            </StyledArticleListItem>
+          </StyledListItem>
+        ))}
+      </StyledTileList>
     </>
   );
 };
---
+++ src/constants/press.ts
@@ -0,0 +1,13 @@
+import { PressArticle } from '../types/press-article';
+import WashingtonPostArticleImage from '../images/washington-post-photograph.png';
+
+export const ARTICLES: PressArticle[] = [
+  {
+    href: 'https://www.washingtonpost.com/technology/2023/02/27/tech-workers-layoffs-quit/',
+    image: WashingtonPostArticleImage,
+    imageAltText:
+      'Jerry Haagsma serves his popcorn, Jerrypop, at an event at Harmonic Brewing in San Francisco on February 18 2023.',
+    publication: 'The Washington Post',
+    title: 'Even before the layoffs, tech workers were quitting. Here’s why.',
+  },
+];
