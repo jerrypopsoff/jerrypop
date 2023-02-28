848ba1f715e40fbdc7d878d9726bed49127e6835
Gerald Haagsma <haagsmag@gmail.com>
2023-02-27 20:49:24 -0800

    Hide recipes page

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -4,7 +4,6 @@ export const NAVIGATION_MENU_ITEMS: NavigationMenuItem[] = [
   { displayName: 'Home', to: '/' },
   { displayName: 'Products', to: '/products' },
   { displayName: 'Order', to: '/order' },
-  { displayName: 'Recipes', to: '/recipes' },
   { displayName: 'About', to: '/about' },
   { displayName: 'Press', to: '/press' },
 ];
