8ced33df50f4a6d3193b6585443247c8ae52c112
Gerald Haagsma <haagsmag@gmail.com>
2023-03-04 16:03:16 -0800

    Add landing URL and redirect for merchandise store

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -1,14 +1,12 @@
 import { NavigationMenuItem } from '../types/navigation';
 
+export const MERCH_HREF = 'https://www.bonfire.com/store/jerrypop';
+
 export const NAVIGATION_MENU_ITEMS: NavigationMenuItem[] = [
   { displayName: 'Home', to: '/' },
   { displayName: 'Products', to: '/products' },
   { displayName: 'Order', to: '/order' },
   { displayName: 'About', to: '/about' },
   { displayName: 'Press', to: '/press' },
-  {
-    displayName: 'Merch',
-    isExternal: true,
-    to: 'https://www.bonfire.com/store/jerrypop',
-  },
+  { displayName: 'Merch', isExternal: true, to: MERCH_HREF },
 ];
