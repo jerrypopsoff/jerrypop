dd62716d51191bf5af6f9a3d4dcb714480a89c4f
Gerald Haagsma <haagsmag@gmail.com>
2023-05-22 15:47:09 -0700

    Extract press articles into a component

--- src/components/Press.tsx
+++ src/components/Press.tsx
@@ -8,67 +8,7 @@ import Baklava2400 from '../images/glamorous-baklava-2400.jpeg';
 import BaklavaWebp600 from '../images/glamorous-baklava-600.webp';
 import BaklavaWebp1200 from '../images/glamorous-baklava-1200.webp';
 import BaklavaWebp2400 from '../images/glamorous-baklava-2400.webp';
-import styled from 'styled-components';
-import {
-  TILE_LIST_STYLE,
-  TILE_LIST_ITEM_STYLE,
-  TILE_IMAGE_CONTAINER_STYLE,
-  TILE_IMAGE_STYLE,
-  TILE_INFORMATION_STYLE,
-} from '../constants/css/tile-list';
-import { ARTICLES } from '../constants/press';
-import {
-  TILE_LIST_BREAKPOINT_WIDTH_PX,
-  TILE_LIST_IMAGE_WIDTH_PX,
-} from '../constants/breakpoint';
-import OptimizedImage from './OptimizedImage';
-
-const StyledTileList = styled.ul`
-  ${TILE_LIST_STYLE}
-`;
-
-const StyledTileListItem = styled.li`
-  ${TILE_LIST_ITEM_STYLE}
-`;
-
-const StyledArticleListItem = styled.a`
-  align-items: center;
-  display: flex;
-  // Margin and padding provide rectangular focus state outline.
-  margin: -8px;
-  padding: 8px;
-  text-decoration: none;
-
-  @media (max-width: ${TILE_LIST_BREAKPOINT_WIDTH_PX}px) {
-    flex-direction: column;
-    margin-left: 0;
-    margin-right: 0;
-    width: 100%;
-  }
-`;
-
-const StyledTileImageContainer = styled.div`
-  ${TILE_IMAGE_CONTAINER_STYLE}
-`;
-
-const StyledTileImage = styled(OptimizedImage)`
-  ${TILE_IMAGE_STYLE}
-`;
-
-const StyledTileInformation = styled.div`
-  ${TILE_INFORMATION_STYLE}
-`;
-
-const StyledArticlePublication = styled(Typography)`
-  font-weight: 300;
-  margin: 0 0 8px;
-  text-transform: uppercase;
-`;
-
-const StyledArticleDate = styled(Typography)`
-  font-size: 12px;
-  margin: 12px 0 0;
-`;
+import PressArticles from './PressArticles';
 
 const Press: React.FC = () => {
   return (
@@ -84,37 +24,7 @@ const Press: React.FC = () => {
         srcSet={`${BaklavaWebp600} 600w, ${BaklavaWebp1200} 1200w, ${BaklavaWebp2400} 2400w`}
       />
       <Typography type="h1">Press</Typography>
-      <StyledTileList>
-        {ARTICLES.map((article) => (
-          <StyledTileListItem key={article.title}>
-            <StyledArticleListItem
-              href={article.href}
-              rel="noreferrer"
-              target="_blank"
-            >
-              <StyledTileImageContainer $aspectRatio={1}>
-                <StyledTileImage
-                  $aspectRatio={1}
-                  alt={article.imageAltText}
-                  fallbackSrc={article.image}
-                  fallbackSrcSet={`${article.image} 600w`}
-                  sizes={`${TILE_LIST_IMAGE_WIDTH_PX}px (min-width: ${TILE_LIST_IMAGE_WIDTH_PX}px), 95vw`}
-                  srcSet={`${article.imageWebp} 600w`}
-                />
-              </StyledTileImageContainer>
-              <StyledTileInformation>
-                <StyledArticlePublication type="p">
-                  {article.publication}
-                </StyledArticlePublication>
-                <Typography margin="0" type="h2">
-                  {article.title}
-                </Typography>
-                <StyledArticleDate type="p">{article.date}</StyledArticleDate>
-              </StyledTileInformation>
-            </StyledArticleListItem>
-          </StyledTileListItem>
-        ))}
-      </StyledTileList>
+      <PressArticles />
     </>
   );
 };
