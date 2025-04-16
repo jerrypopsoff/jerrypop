190b8a7832129a536caf8241b7dbfa22fa0fb62f
Gerald Haagsma <haagsmag@gmail.com>
2025-04-15 23:54:40 -0700

    Specify Cypress base URL in configuration file

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   blockHosts: ['*google-analytics.com', '*googletagmanager.com'],
   e2e: {
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(_on, _config) {
       // Implement node event listeners here.
     },
