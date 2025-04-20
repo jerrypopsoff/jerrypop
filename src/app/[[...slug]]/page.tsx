430f130fdaa43eaa22808de2d18f7ef610f56d6c
Gerald Haagsma <haagsmag@gmail.com>
2025-04-20 11:39:29 -0700

    Create entrypoint page

---
+++ src/app/[[...slug]]/page.tsx
@@ -0,0 +1,12 @@
+/**
+ * This tells Next.js to generate a single route for the empty slug (/),
+ * effectively mapping all routes to the same page. This page is a Server
+ * Component, prerendered into static HTML.
+ */
+export function generateStaticParams() {
+  return [{ slug: [''] }];
+}
+
+export default function Page() {
+  return '...';
+}
