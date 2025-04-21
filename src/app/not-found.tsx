3064aa8fbb98478f20e23a087c8d11117ea81f2c
Gerald Haagsma <haagsmag@gmail.com>
2025-04-21 00:21:44 -0700

    Move not found metadata back to client
    For some reason the page title doesn't get properly applied when using server-side metadata

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,4 @@
-import { Metadata } from 'next';
-import { getMetadata } from '../utilities/metadata';
 import { ClientOnly } from './[[...slug]]/client';
-
-export const metadata: Metadata = {
-  ...getMetadata(),
-  title: 'Page Not Found · Jerrypop',
-};
 
 export default function Page() {
   return <ClientOnly />;
--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,8 +1,12 @@
+import { Helmet } from 'react-helmet-async';
 import Typography from './Typography';
 
 const NotFound = () => {
   return (
     <>
+      <Helmet>
+        <title>Page Not Found · Jerrypop</title>
+      </Helmet>
       <Typography type="h1">Ope!</Typography>
       <Typography type="p">
         Someone spilled the kernels. That page does not exist.
