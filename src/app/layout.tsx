3be6096e4b1f7682131316663ddf721b4a929b48
Jerry Haagsma <haagsmag@gmail.com>
2025-05-16 01:54:05 -0700

    Integrate click pop and console ascii art into app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { getViewport } from '../utilities/viewport';
 import { isProduction } from '../utilities/environment';
 import { GLOBAL_STYLE as GlobalStyle } from '../constants/css/global-style';
 import StyledComponentsRegistry from '../components/StyledComponentsRegistry';
+import ClickPop from '../components/ClickPop';
+import ConsoleAsciiArt from '../components/ConsoleAsciiArt';
 import { ThemeProvider } from 'next-themes';
 
 export const metadata = getMetadata();
@@ -28,6 +30,8 @@ export default function RootLayout({
         <StyledComponentsRegistry>
           <ThemeProvider>
             <GlobalStyle />
+            <ConsoleAsciiArt />
+            <ClickPop />
             {children}
           </ThemeProvider>
         </StyledComponentsRegistry>
