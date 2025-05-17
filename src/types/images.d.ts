972331ff6367667b31e4900ea4ea3518fd25cb53
Jerry Haagsma <haagsmag@gmail.com>
2025-05-17 00:42:00 -0700

    Attempt union type for image module declarations

--- src/types/images.d.ts
+++ src/types/images.d.ts
@@ -1,26 +1,26 @@
 import type { SourceSet } from './image';
 
 declare module '*.png' {
-  const value: SourceSet;
+  const value: SourceSet | string;
   export default value;
 }
 
 declare module '*.jpg' {
-  const value: SourceSet;
+  const value: SourceSet | string;
   export default value;
 }
 
 declare module '*.jpeg' {
-  const value: SourceSet;
+  const value: SourceSet | string;
   export default value;
 }
 
 declare module '*.svg' {
-  const value: SourceSet;
+  const value: SourceSet | string;
   export default value;
 }
 
 declare module '*.webp' {
-  const value: SourceSet;
+  const value: SourceSet | string;
   export default value;
 }
