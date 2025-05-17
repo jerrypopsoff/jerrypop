fe453d07a1eb44c87f4f761cf92b1d76e96076b2
Jerry Haagsma <haagsmag@gmail.com>
2025-05-17 14:42:43 -0700

    Add unit test for display currency range utility

--- src/utilities/currency.test.ts
+++ src/utilities/currency.test.ts
@@ -10,5 +10,13 @@ describe('utilities/currency', () => {
       expect(displayCurrency(5.5)).toEqual('$5.50');
       expect(displayCurrency(8)).toEqual('$8.00');
     });
+
+    test('it displays user-friendly currency ranges', () => {
+      expect(displayCurrency({ min: 0, max: 1 })).toEqual('$0.00 - $1.00');
+      expect(displayCurrency({ min: 5, max: 10 })).toEqual('$5.00 - $10.00');
+      expect(displayCurrency({ min: -5, max: 5 })).toEqual('-$5.00 - $5.00');
+      expect(displayCurrency({ min: -5, max: -1 })).toEqual('-$5.00 - -$1.00');
+      expect(displayCurrency({ min: 0, max: 0 })).toEqual('$0.00 - $0.00');
+    });
   });
 });
