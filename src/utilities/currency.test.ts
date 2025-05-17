87a79dc88a59d978ae50cf06e30c2944a5215258
Jerry Haagsma <haagsmag@gmail.com>
2025-05-17 14:14:14 -0700

    Add unit test for non-trivial utiility modules

--- src/utilities/currency.test.ts
+++ src/utilities/currency.test.ts
@@ -2,11 +2,13 @@ import { describe, expect, test } from 'vitest';
 import { displayCurrency } from './currency';
 
 describe('utilities/currency', () => {
-  test('it displays currency values for en-US locale', () => {
-    expect(displayCurrency(0)).toEqual('$0.00');
-    expect(displayCurrency(50)).toEqual('$50.00');
-    expect(displayCurrency(-50)).toEqual('-$50.00');
-    expect(displayCurrency(5.5)).toEqual('$5.50');
-    expect(displayCurrency(8)).toEqual('$8.00');
+  describe('displayCurrency', () => {
+    test('it displays user-friendly currency values', () => {
+      expect(displayCurrency(0)).toEqual('$0.00');
+      expect(displayCurrency(50)).toEqual('$50.00');
+      expect(displayCurrency(-50)).toEqual('-$50.00');
+      expect(displayCurrency(5.5)).toEqual('$5.50');
+      expect(displayCurrency(8)).toEqual('$8.00');
+    });
   });
 });
