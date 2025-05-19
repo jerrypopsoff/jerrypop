3daf65c900f792d21eed5c4743972fcac641710e
Jerry Haagsma <haagsmag@gmail.com>
2025-05-18 17:03:44 -0700

    Update application test to verify interactive root layout elements are displayed on all pages

--- cypress/e2e/application.cy.ts
+++ cypress/e2e/application.cy.ts
@@ -1,10 +1,11 @@
 import { RoutePath } from '../types/route-path';
 
-describe('general application tests', () => {
-  it('displays a page heading and footer on every page', () => {
+describe('application tests', () => {
+  it('displays root layout elements on every page', () => {
     Object.values(RoutePath).forEach((path) => {
       cy.visit(path, { failOnStatusCode: path !== RoutePath.NotFound });
 
+      cy.get('nav').should('have.length', 2); // (desktop and mobile)
       cy.get('h1').should('have.length.least', 1);
       cy.get('footer').should('have.length', 1);
     });
