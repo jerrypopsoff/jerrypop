83387e0c98716538e388d88ea84a30326dc7acd6
Jerry Haagsma <haagsmag@gmail.com>
2025-05-19 20:36:05 -0700

    Migrate SocialLinks component to Tailwind

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -2,7 +2,6 @@ import EmailIconDark from '../images/email-icon-dark.svg';
 import FacebookIconDark from '../images/facebook-icon-dark.svg';
 import InstagramIconDark from '../images/instagram-icon-dark.svg';
 import YouTubeIconDark from '../images/youtube-icon-dark.svg';
-import styled from 'styled-components';
 import {
   EMAIL_URL,
   FACEBOOK_PROFILE_URL,
@@ -12,84 +11,74 @@ import {
 import Image from 'next/image';
 import Link from 'next/link';
 
-const StyledSocialLinksList = styled.ul`
-  align-items: center;
-  display: flex;
-  list-style: none;
-  margin: 0;
-  padding: 0;
-`;
-
-const StyledSocialLinksListItem = styled.li`
-  margin: 0 4px;
-`;
-
-const StyledSocialLink = styled(Link)`
-  display: flex;
-  padding: 4px;
-`;
-
-const StyledSocialLinkIcon = styled(Image)`
-  height: 24px;
-  width: 24px;
-`;
-
 const SocialLinks = () => {
   return (
-    <StyledSocialLinksList>
-      <StyledSocialLinksListItem>
-        <StyledSocialLink
+    <ul className="flex">
+      <li className="mx-1">
+        <Link
+          className="block p-1"
           href={FACEBOOK_PROFILE_URL}
           target="_blank"
           rel="noreferrer"
         >
-          <StyledSocialLinkIcon
+          <Image
             alt="Jerrypop on Facebook"
+            className="h-6 w-6"
             height={24}
             src={FacebookIconDark.src}
             width={24}
           />
-        </StyledSocialLink>
-      </StyledSocialLinksListItem>
-      <StyledSocialLinksListItem>
-        <StyledSocialLink
+        </Link>
+      </li>
+      <li className="mx-1">
+        <Link
+          className="block p-1"
           href={INSTAGRAM_PROFILE_URL}
           target="_blank"
           rel="noreferrer"
         >
-          <StyledSocialLinkIcon
+          <Image
             alt="Jerrypop on Instagram"
+            className="h-6 w-6"
             height={24}
             src={InstagramIconDark.src}
             width={24}
           />
-        </StyledSocialLink>
-      </StyledSocialLinksListItem>
-      <StyledSocialLinksListItem>
-        <StyledSocialLink
+        </Link>
+      </li>
+      <li className="mx-1">
+        <Link
+          className="block p-1"
           href={YOU_TUBE_CHANNEL_URL}
           target="_blank"
           rel="noreferrer"
         >
-          <StyledSocialLinkIcon
+          <Image
             alt="Jerrypop on YouTube"
+            className="h-6 w-6"
             height={24}
             src={YouTubeIconDark.src}
             width={24}
           />
-        </StyledSocialLink>
-      </StyledSocialLinksListItem>
-      <StyledSocialLinksListItem>
-        <StyledSocialLink href={EMAIL_URL} target="_blank" rel="noreferrer">
-          <StyledSocialLinkIcon
+        </Link>
+      </li>
+      <li className="mx-1">
+        <Link
+          className="block p-1"
+          href={EMAIL_URL}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Image
             alt="Email Jerrypop"
+            className="h-6 w-6"
             height={24}
             src={EmailIconDark.src}
             width={24}
           />
-        </StyledSocialLink>
-      </StyledSocialLinksListItem>
-    </StyledSocialLinksList>
+        </Link>
+      </li>
+    </ul>
   );
 };
 
