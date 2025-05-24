84bf4d8d6264c20556153c2581d826b773bb48b6
Jerry Haagsma <haagsmag@gmail.com>
2025-05-24 11:33:32 -0700

    Hide mobile navigation menu items after menu close animation

--- src/components/MobileNavigationBar.tsx
+++ src/components/MobileNavigationBar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import WordmarkSvgDark from '../images/jerrypop-wordmark-soft-white.svg';
 import { NavigationMenuItem } from '../types/navigation';
 import Image from 'next/image';
@@ -18,34 +18,60 @@ interface Props {
 
 const MobileNavigationBar = ({ navigationMenuItems }: Props) => {
   const [isOpen, setIsOpen] = useState(false);
-  const toggleMenuAriaLabel = isOpen
+  const [menuItemsClassName, setMenuItemsClassName] = useState(
+    isOpen ? undefined : 'invisible',
+  );
+
+  /**
+   * If the menu is closed, set the class to 'invisible' after a delay matching
+   * the transition duration (300ms). This prevents the menu items from being
+   * visible when the menu is closed when scrolling above the top of the page on
+   * desktop browsers.
+   */
+  useEffect(() => {
+    if (isOpen) {
+      setMenuItemsClassName(undefined);
+      return;
+    }
+
+    const timeoutId = setTimeout(() => {
+      setMenuItemsClassName('invisible');
+    }, 300);
+
+    return () => clearTimeout(timeoutId);
+  }, [isOpen]);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+  const closeMenu = () => setIsOpen(false);
+
+  const slideableTransform = isOpen ? 'translate-y-[21rem]' : 'translate-y-0';
+  const hamburgerButtonAriaLabel = isOpen
     ? 'Close navigation menu'
     : 'Open navigation menu';
+  const menuItemsAriaHidden = isOpen ? undefined : 'true';
+  const menuItemTabIndex = isOpen ? 0 : -1;
 
   return (
     <nav className="sm:hidden">
       <div
-        className="
+        className={`
           z-nav bg-background z-nav absolute left-0 right-0 top-[-21rem] flex
           flex-col-reverse justify-center transition-transform duration-300
-          ease-in-out
-        "
-        style={{
-          transform: isOpen ? 'translateY(21rem)' : 'translateY(0)',
-        }}
+          ease-in-out ${slideableTransform}
+        `}
       >
         <div className="flex justify-center py-5">
           <button
-            aria-label={toggleMenuAriaLabel}
+            aria-label={hamburgerButtonAriaLabel}
             className="
               column z-hamburger absolute left-0 flex flex-col justify-around
               p-5
             "
-            onClick={() => setIsOpen(!isOpen)}
+            onClick={toggleMenu}
           >
             <HamburgerIcon className="h-5 w-5" />
           </button>
-          <Link className="p-4" onClick={() => setIsOpen(false)} href="/">
+          <Link className="p-4" onClick={closeMenu} href="/">
             <Image
               alt="Jerrypop brand wordmark"
               className="w-31 h-7"
@@ -55,13 +81,13 @@ const MobileNavigationBar = ({ navigationMenuItems }: Props) => {
             />
           </Link>
         </div>
-        <ul aria-hidden={isOpen ? undefined : 'true'}>
+        <ul aria-hidden={menuItemsAriaHidden} className={menuItemsClassName}>
           <li>
             <button
               className="w-full p-5 text-left text-xl font-semibold"
               aria-label="Close navigation menu"
-              onClick={() => setIsOpen(!isOpen)}
-              tabIndex={isOpen ? 0 : -1}
+              onClick={toggleMenu}
+              tabIndex={menuItemTabIndex}
             >
               <CloseIcon className="h-4 w-4" />
             </button>
@@ -71,9 +97,9 @@ const MobileNavigationBar = ({ navigationMenuItems }: Props) => {
               <Link
                 className="block px-5 py-4 uppercase tracking-wide no-underline"
                 href={href}
-                onClick={() => setIsOpen(false)}
+                onClick={closeMenu}
                 rel={isExternal ? 'noreferrer' : undefined}
-                tabIndex={isOpen ? 0 : -1}
+                tabIndex={menuItemTabIndex}
                 target={isExternal ? '_blank' : undefined}
               >
                 {displayName}
