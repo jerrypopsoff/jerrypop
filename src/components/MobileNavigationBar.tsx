44a29679dba5c9ae604967e8fcb0b2ca0420cf47
Jerry Haagsma <haagsmag@gmail.com>
2025-05-24 10:58:45 -0700

    Use SVG icons instead of text characters and CSS for mobile navigation menu toggle buttons

--- src/components/MobileNavigationBar.tsx
+++ src/components/MobileNavigationBar.tsx
@@ -5,6 +5,12 @@ import { useState } from 'react';
 import WordmarkSvgDark from '../images/jerrypop-wordmark-soft-white.svg';
 import { NavigationMenuItem } from '../types/navigation';
 import Image from 'next/image';
+import HamburgerIcon from './icons/HamburgerIcon';
+import dynamic from 'next/dynamic';
+
+const CloseIcon = dynamic(() => import('./icons/CloseIcon'), {
+  ssr: false,
+});
 
 interface Props {
   navigationMenuItems: NavigationMenuItem[];
@@ -12,31 +18,32 @@ interface Props {
 
 const MobileNavigationBar = ({ navigationMenuItems }: Props) => {
   const [isOpen, setIsOpen] = useState(false);
+  const toggleMenuAriaLabel = isOpen
+    ? 'Close navigation menu'
+    : 'Open navigation menu';
 
   return (
     <nav className="sm:hidden">
       <div
         className="
-          z-nav bg-background z-nav absolute left-0 right-0 top-[-21.5rem] flex
+          z-nav bg-background z-nav absolute left-0 right-0 top-[-21rem] flex
           flex-col-reverse justify-center transition-transform duration-300
           ease-in-out
         "
         style={{
-          transform: isOpen ? 'translateY(21.5rem)' : 'translateY(0)',
+          transform: isOpen ? 'translateY(21rem)' : 'translateY(0)',
         }}
       >
         <div className="flex justify-center py-5">
           <button
-            aria-label="Toggle navigation menu"
+            aria-label={toggleMenuAriaLabel}
             className="
-            column h-15 w-15 z-hamburger absolute left-1 flex flex-col
-            justify-around p-5
-          "
+              column z-hamburger absolute left-0 flex flex-col justify-around
+              p-5
+            "
             onClick={() => setIsOpen(!isOpen)}
           >
-            <div aria-hidden="true" className="bg-foreground h-0.5 w-full" />
-            <div aria-hidden="true" className="bg-foreground h-0.5 w-full" />
-            <div aria-hidden="true" className="bg-foreground h-0.5 w-full" />
+            <HamburgerIcon className="h-5 w-5" />
           </button>
           <Link className="p-4" onClick={() => setIsOpen(false)} href="/">
             <Image
@@ -51,18 +58,18 @@ const MobileNavigationBar = ({ navigationMenuItems }: Props) => {
         <ul aria-hidden={isOpen ? undefined : 'true'}>
           <li>
             <button
-              className="w-full px-6 py-4 text-left text-lg font-semibold"
+              className="w-full p-5 text-left text-xl font-semibold"
               aria-label="Close navigation menu"
               onClick={() => setIsOpen(!isOpen)}
               tabIndex={isOpen ? 0 : -1}
             >
-              ✕
+              <CloseIcon className="h-4 w-4" />
             </button>
           </li>
           {navigationMenuItems.map(({ displayName, href, isExternal }) => (
             <li key={displayName}>
               <Link
-                className="block px-6 py-4 uppercase tracking-wide no-underline"
+                className="block px-5 py-4 uppercase tracking-wide no-underline"
                 href={href}
                 onClick={() => setIsOpen(false)}
                 rel={isExternal ? 'noreferrer' : undefined}
---
+++ src/components/icons/HamburgerIcon.tsx
@@ -0,0 +1,16 @@
+export default function HamburgerIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={2}
+      {...props}
+    >
+      <line x1="0" y1="4" x2="24" y2="4" />
+      <line x1="0" y1="12" x2="24" y2="12" />
+      <line x1="0" y1="20" x2="24" y2="20" />
+    </svg>
+  );
+}
