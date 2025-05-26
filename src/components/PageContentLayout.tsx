5645cc0dd0c10d7a8254b9fd65eb7bb719cc39df
Jerry Haagsma <haagsmag@gmail.com>
2025-05-26 00:51:25 -0700

    Fix print width violation

--- src/components/PageContentLayout.tsx
+++ src/components/PageContentLayout.tsx
@@ -4,7 +4,11 @@ export default function PageContentLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="max-w-158 mx-auto flex flex-col items-center px-4 text-center">
+    <div
+      className="
+        max-w-158 mx-auto flex flex-col items-center px-4 text-center
+      "
+    >
       {children}
     </div>
   );
