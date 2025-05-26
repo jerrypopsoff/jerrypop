8a2b2498ab4ba30a3cc0dd012f154dae669d7ae9
Jerry Haagsma <haagsmag@gmail.com>
2025-05-25 23:11:19 -0700

    Migrate NotFound page to Tailwind

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,24 +1,26 @@
 'use client';
 
-import { styled } from 'styled-components';
 import Heading1 from './common/Heading1';
+import PageHeaderPhotograph from './common/PageHeaderPhotograph';
+import PageHeaderPhotographSrc from '../images/glamorous-pbj-sheet.jpeg';
 import Paragraph from './common/Paragraph';
-
-const StyledNotFound = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  text-align: center;
-`;
+import PageContentLayout from './PageContentLayout';
 
 const NotFound = () => {
   return (
-    <StyledNotFound>
-      <Heading1>Ope!</Heading1>
-      <Paragraph>
-        Someone spilled the kernels. That page does not exist.
-      </Paragraph>
-    </StyledNotFound>
+    <>
+      <PageHeaderPhotograph
+        alt="Photograph of PB&J Craft Popcorn spread onto a baking sheet"
+        aspectRatioCss="4032 / 3024"
+        src={PageHeaderPhotographSrc}
+      />
+      <PageContentLayout>
+        <Heading1>Ope!</Heading1>
+        <Paragraph>
+          Someone spilled the kernels. That page does not exist.
+        </Paragraph>
+      </PageContentLayout>
+    </>
   );
 };
 
