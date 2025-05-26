bb52c75bb5d4325775cefa1d7ccefc7d597c708c
Jerry Haagsma <haagsmag@gmail.com>
2025-05-26 00:15:49 -0700

    Migrate Recipes page to Tailwind

--- src/components/Recipes.tsx
+++ src/components/Recipes.tsx
@@ -4,39 +4,28 @@ import { RECIPES } from '../constants/recipe';
 import Recipe from './Recipe';
 import Heading1 from './common/Heading1';
 import Paragraph from './common/Paragraph';
-import styled from 'styled-components';
 import PageHeaderPhotographSrc from '../images/glamorous-pbj-sheet.jpeg';
 import PageHeaderPhotograph from './common/PageHeaderPhotograph';
-
-const StyledRecipes = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  text-align: center;
-`;
-
-const StyledUnorderedList = styled.ul`
-  padding: 0;
-  list-style: none;
-  margin: 0;
-`;
+import PageContentLayout from './PageContentLayout';
 
 const Recipes = () => {
   return (
-    <StyledRecipes>
+    <>
       <PageHeaderPhotograph
         alt="Photograph of PB&J Craft Popcorn spread onto a baking sheet"
         aspectRatioCss="4032 / 3024"
         src={PageHeaderPhotographSrc}
       />
-      <Heading1>Recipes</Heading1>
-      <Paragraph>Select a recipe to view details.</Paragraph>
-      <StyledUnorderedList>
-        {RECIPES.map((recipe) => {
-          return <Recipe key={recipe.title} recipe={recipe} />;
-        })}
-      </StyledUnorderedList>
-    </StyledRecipes>
+      <PageContentLayout>
+        <Heading1>Recipes</Heading1>
+        <Paragraph>Select a recipe to view details.</Paragraph>
+        <ul className="px-12.5 w-full">
+          {RECIPES.map((recipe) => {
+            return <Recipe key={recipe.title} recipe={recipe} />;
+          })}
+        </ul>
+      </PageContentLayout>
+    </>
   );
 };
 
