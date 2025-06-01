50badc4d112b917947a829c46ef7d5fe3a2a32c3
Jerry Haagsma <haagsmag@gmail.com>
2025-06-01 14:01:51 -0700

    Manually capture page view events to support client-side navigation

--- src/components/PostHogProvider.tsx
+++ src/components/PostHogProvider.tsx
@@ -1,9 +1,11 @@
 'use client';
 
-import { useEffect } from 'react';
+import { usePathname, useSearchParams } from 'next/navigation';
+import { useEffect, Suspense } from 'react';
 import posthog from 'posthog-js';
 import { PostHogProvider as PHProvider } from 'posthog-js/react';
 import { isProduction } from '../utilities/environment';
+import { usePostHog } from 'posthog-js/react';
 
 function handleMissingApiKey() {
   if (!isProduction()) {
@@ -11,6 +13,42 @@ function handleMissingApiKey() {
   }
 
   console.error('Attempted to initialize PostHog without a valid API key.');
+}
+
+function PostHogPageView() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const posthog = usePostHog();
+
+  useEffect(() => {
+    if (!pathname || !posthog) {
+      return;
+    }
+
+    const searchParamsString = searchParams.toString();
+    const url =
+      searchParamsString.length > 0
+        ? `${window.origin}${pathname}?${searchParamsString}`
+        : `${window.origin}${pathname}`;
+
+    posthog.capture('$pageview', { $current_url: url });
+  }, [pathname, searchParams, posthog]);
+
+  return null;
+}
+
+/**
+ * Wrap `PostHogPageView` in Suspense to avoid the `useSearchParams` usage from
+ * de-opting the whole application into client-side rendering.
+ *
+ * https://nextjs.org/docs/messages/deopted-into-client-rendering
+ */
+function SuspendedPostHogPageView() {
+  return (
+    <Suspense fallback={null}>
+      <PostHogPageView />
+    </Suspense>
+  );
 }
 
 export function PostHogProvider({
@@ -30,11 +68,17 @@ export function PostHogProvider({
       api_host: 'https://us.i.posthog.com',
       capture_exceptions: true,
       capture_heatmaps: true,
+      capture_pageview: false,
       capture_performance: true,
       persistence: 'memory',
       person_profiles: 'never',
     });
   }, [apiKey]);
 
-  return <PHProvider client={posthog}>{children}</PHProvider>;
+  return (
+    <PHProvider client={posthog}>
+      <SuspendedPostHogPageView />
+      {children}
+    </PHProvider>
+  );
 }
