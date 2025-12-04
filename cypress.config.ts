fd88d43c200e8ed53946abe4e202cede0f9d482c
Jerry Haagsma <haagsmag@gmail.com>
2025-12-03 21:47:45 -0800

    Finish Google Analytics cleanup (#461)
    * Uninstall Google Analytics types
    
    * Replace Google Analytics blocked hosts with Posthog for Cypress e2e tests
    
    * Remove blocked hosts since Posthog is disabled in previews

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  blockHosts: ['*google-analytics.com', '*googletagmanager.com'],
   e2e: {
     baseUrl: 'http://localhost:3000',
     defaultCommandTimeout: 2000,
