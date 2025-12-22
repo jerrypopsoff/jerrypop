1d4c8de67632659552abacfdb1feeaab3201f417
Jerry Haagsma <haagsmag@gmail.com>
2025-12-22 11:54:11 -0800

    Prevent search engines from indexing and following links on the gift page

--- src/app/gift/page.tsx
+++ src/app/gift/page.tsx
@@ -2,6 +2,10 @@ import { Gift } from '../../components/Gift';
 
 export const metadata = {
   title: 'A Special Gift for Gikui · Jerrypop',
+  robots: {
+    index: false,
+    follow: false,
+  },
 };
 
 export default function Page() {
