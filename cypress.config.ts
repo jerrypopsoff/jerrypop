d9c7e6b1933dc262802c9c2f5e75042abe81e466
Jerry Haagsma <haagsmag@gmail.com>
2026-02-09 22:30:09 -0800

    Upgrade Cypress to 15.10.0 (#467)
    * Upgrade Cypress
    
    * Remove timeout overrides to be more forgiving
    
    This is necessary to support longer load times due to local turbopack and CI coldstarts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  /**
+   * Disable the Cypress environment variable feature, which is not supported
+   * by the Cypress 15.10.0 release.
+   */
+  allowCypressEnv: false,
   e2e: {
     baseUrl: 'http://localhost:3000',
-    defaultCommandTimeout: 2000,
-    pageLoadTimeout: 5000,
   },
   projectId: 'ttcbvc',
 });
